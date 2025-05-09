139102d40d162053e70448940fedd07b4c6974e5
Illia <itdoctor@gmail.com>
2025-05-09 12:45:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let numberOfFilms = +prompt("сколько фильмов вы уже пересмотрели?", ""); //задаем вопрос пользователю
+
+let personaMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+console.log(personaMovieDB);
