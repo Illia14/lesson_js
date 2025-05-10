97d871eb95549b8c4d6e000086d2373a43bfe9bc
Illia <itdoctor@gmail.com>
2025-05-10 16:58:49 +0200

    Условие

---
+++ uslowie.js
@@ -0,0 +1,36 @@
+"use script"
+
+if (1) {
+    console.log("ok");
+} else {
+    console.log('error')
+}
+
+
+// if (num < 49) {
+//     console.log ('error');
+// } else if (num > 100) {
+//     console.log ('mnogo');
+// } else {
+//     console.log('ok');
+// }
+
+// (num === 50) ? console.log('ok') : console.log ('error');
+
+
+const num = '50';
+
+switch (num) {
+        case '49':
+            console.log('неверно ');
+            break;
+        case '100':
+            console.log('неверно ');
+            break;
+        case '50':
+            console.log('в точку ');
+            break;
+        default:
+            console.log('не в этот раз');
+            break;
+}
